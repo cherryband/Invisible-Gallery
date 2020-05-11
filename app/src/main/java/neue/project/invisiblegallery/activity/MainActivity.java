cf52337fa5b3a408e05c50258615e6f0be360d4d
Jihoon Kim <imsesaok@tuta.io>
2020-05-11 23:02:56 +0800

    Code reformatting; Directory restructuring
    Signed-off-by: Jihoon Kim <imsesaok@tuta.io>

--- app/src/main/java/neue/project/invisiblegallery/activity/MainActivity.java
+++ app/src/main/java/neue/project/invisiblegallery/activity/MainActivity.java
@@ -1,18 +1,23 @@
-package neue.project.invisiblegallery;
+package neue.project.invisiblegallery.activity;
 
 import android.os.Bundle;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import neue.project.invisiblegallery.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -20,14 +25,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu (Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected (MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
